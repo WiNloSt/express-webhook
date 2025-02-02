a7515a608e0ffd4a83298a8e122bc43a6f2b2734
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-02-02 18:41:34 +0700

    Add a basic webhook server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const compression = require('compression')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(compression())
+
+const wiseWebhookRouter = express.Router()
+wiseWebhookRouter.use(bodyParser.text({ type: '*/*' }))
+app.use('/wise-webhook', wiseWebhookRouter)
+wiseWebhookRouter.post('/', function (req, res) {
+  const originalBody = req.body
+  try {
+    const jsonBody = JSON.parse(originalBody)
+    res.send('success')
+  } catch (_error) {
+    res.status(400).send('Invalid JSON')
+  }
+})
+
+app.listen(3000)
