faf02c1da1a9b71c9d23bd95097d18791df29079
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-02-08 15:19:51 +0700

    Add request verification

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const compression = require('compression')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const { isVerified } = require('./verify')
 
 const app = express()
 app.use(compression())
@@ -12,12 +13,20 @@ wiseWebhookRouter.use(bodyParser.text({ type: '*/*' }))
 app.use('/wise-webhook', wiseWebhookRouter)
 wiseWebhookRouter.post('/', function (req, res) {
   const originalBody = req.body
+  const signatureHeader = req.headers['x-signature-sha256']
   try {
     const jsonBody = JSON.parse(originalBody)
+    if (isVerified(originalBody, signatureHeader)) {
+      console.log('body', JSON.stringify(jsonBody, null, 2))
+    } else {
+      console.error('Invalid signature')
+      return res.status(400).send('Invalid signature')
+    }
     res.send('success')
   } catch (_error) {
     res.status(400).send('Invalid JSON')
   }
 })
 
+console.log('Server started at http://localhost:3000')
 app.listen(3000)
---
+++ verify.js
@@ -0,0 +1,34 @@
+const crypto = require('crypto')
+
+const PRODUCTION_SIGNATURE = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvO8vXV+JksBzZAY6GhSO
+XdoTCfhXaaiZ+qAbtaDBiu2AGkGVpmEygFmWP4Li9m5+Ni85BhVvZOodM9epgW3F
+bA5Q1SexvAF1PPjX4JpMstak/QhAgl1qMSqEevL8cmUeTgcMuVWCJmlge9h7B1CS
+D4rtlimGZozG39rUBDg6Qt2K+P4wBfLblL0k4C4YUdLnpGYEDIth+i8XsRpFlogx
+CAFyH9+knYsDbR43UJ9shtc42Ybd40Afihj8KnYKXzchyQ42aC8aZ/h5hyZ28yVy
+Oj3Vos0VdBIs/gAyJ/4yyQFCXYte64I7ssrlbGRaco4nKF3HmaNhxwyKyJafz19e
+HwIDAQAB
+-----END PUBLIC KEY-----
+`
+exports.isVerified = function isVerified(rawBody, signatureHeader) {
+  if (!signatureHeader) {
+    return false
+  }
+
+  const publicKey = crypto.createPublicKey({
+    key: PRODUCTION_SIGNATURE,
+    format: 'pem',
+  })
+
+  const isVerified = crypto.verify(
+    'RSA-SHA256',
+    Buffer.from(rawBody),
+    {
+      key: publicKey,
+      padding: crypto.constants.RSA_PKCS1_PADDING,
+    },
+    Buffer.from(signatureHeader, 'base64')
+  )
+
+  return isVerified
+}
